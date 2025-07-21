2b8072cbd07a3e5cd86538bbf85f762eaa076011
ayyanar-vignesh <ayyanar.2481996@gmail.com>
2025-07-21 23:02:38 +0530

    updated main_tf code

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,19 @@
 # VPC
-resource "aws_vpc" "main_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = { Name = "july21-fastapi-vpc" }
+data "aws_vpc" "main_vpc" {
+
+  id = "vpc-0cf9552d0478613b6"
+  
 }
 
 # Internet Gateway
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main_vpc.id
+  vpc_id = data.aws_vpc.main_vpc.id
   tags   = { Name = "fastapi-igw" }
 }
 
 # Public Subnet
 resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main_vpc.id
+  vpc_id                  = data.aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "ap-south-1a"
@@ -23,7 +22,7 @@ resource "aws_subnet" "public_subnet" {
 
 # Route Table
 resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main_vpc.id
+  vpc_id = data.aws_vpc.main_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -41,7 +40,7 @@ resource "aws_route_table_association" "public_assoc" {
 resource "aws_security_group" "fastapi_sg" {
   name        = "fastapi-sg"
   description = "Allow SSH and FastAPI access"
-  vpc_id      = aws_vpc.main_vpc.id
+  vpc_id      = data.aws_vpc.main_vpc.id
 
   ingress {
     from_port   = 22
