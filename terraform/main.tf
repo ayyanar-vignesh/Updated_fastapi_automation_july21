e5893e886af8b5b52cebe9c565cc7bb92a1eadd0
ayyanar-vignesh <ayyanar.2481996@gmail.com>
2025-07-21 23:09:37 +0530

    updated 2309

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,85 +1,86 @@
-# VPC
+provider "aws" {
+  region = "ap-south-1"
+}
+
+####################
+# VPC (use existing)
+####################
 data "aws_vpc" "main_vpc" {
-
   id = "vpc-0cf9552d0478613b6"
-  
 }
 
-# Internet Gateway
-resource "aws_internet_gateway" "igw" {
+##############################
+# Internet Gateway (reuse if exists)
+##############################
+data "aws_internet_gateway" "existing_igw" {
+  filter {
+    name   = "attachment.vpc-id"
+    values = [data.aws_vpc.main_vpc.id]
+  }
+}
+
+####################
+# Subnet (reuse if exists)
+####################
+data "aws_subnet" "existing_public_subnet" {
+  filter {
+    name   = "cidr-block"
+    values = ["10.0.1.0/24"]
+  }
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main_vpc.id]
+  }
+}
+
+##########################
+# Security Group (reuse if exists)
+##########################
+data "aws_security_group" "existing_sg" {
+  filter {
+    name   = "group-name"
+    values = ["fastapi-sg"]
+  }
+
   vpc_id = data.aws_vpc.main_vpc.id
-  tags   = { Name = "fastapi-igw" }
 }
 
-# Public Subnet
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = data.aws_vpc.main_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "ap-south-1a"
-  tags                    = { Name = "july21-public-subnet" }
-}
-
-# Route Table
+####################
+# Route Table (create if needed)
+####################
 resource "aws_route_table" "public_rt" {
   vpc_id = data.aws_vpc.main_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-# Associate Route Table
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-# Security Group
-resource "aws_security_group" "fastapi_sg" {
-  name        = "fastapi-sg"
-  description = "Allow SSH and FastAPI access"
-  vpc_id      = data.aws_vpc.main_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    gateway_id = data.aws_internet_gateway.existing_igw.id
   }
 
   tags = {
-    Name = "fastapi-security-group"
+    Name = "july21-public-rt"
   }
 }
 
+##########################
+# Route Table Association
+##########################
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = data.aws_subnet.existing_public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
 
+####################
 # EC2 Instance
+####################
 resource "aws_instance" "fastapi_ec2" {
-  ami                         = "ami-03f4878755434977f" # Ubuntu 22.04 in ap-south-1
+  ami                         = "ami-03f4878755434977f" # Ubuntu 22.04 for ap-south-1
   instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.public_subnet.id
-  vpc_security_group_ids      = [aws_security_group.fastapi_sg.id]
+  subnet_id                   = data.aws_subnet.existing_public_subnet.id
+  vpc_security_group_ids      = [data.aws_security_group.existing_sg.id]
   associate_public_ip_address = true
   key_name                    = var.key_name
 
   tags = {
     Name = "FastAPI-EC2-july21"
   }
-
 }
