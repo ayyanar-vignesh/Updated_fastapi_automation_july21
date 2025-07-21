0155852e2e02cbc2e935f51685ac46cef4294051
ayyanar-vignesh <ayyanar.2481996@gmail.com>
2025-07-22 00:33:36 +0530

    changed 1233

--- user_data.sh
+++ user_data.sh
@@ -11,5 +11,6 @@ cd app
 # Install required Python packages
 pip3 install fastapi uvicorn boto3 python-dotenv jinja2 python-multipart bcrypt
 
+
 # Run the FastAPI app in the background on port 8000
 nohup uvicorn main:app --host 0.0.0.0 --port 8000 > fastapi.log 2>&1 &
